7ee39dd9b87bdc6132399115940aa891dc592396
rajpoatar <rajpoatar@gmail.com>
2025-12-27 22:19:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?D3m0jE'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?axrlWc'
 };
